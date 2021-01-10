e2b8709ae18a198ea8537821ea7f8e2165928d99
Lee2532 <seungoh0423@gmail.com>
2021-01-10 11:48:26 +0900

    arrow function study

--- jobs/static/js/test.js
+++ jobs/static/js/test.js
@@ -1,6 +1,10 @@
 function funcName() { console.log('arg') };
 
-function test() {
+function test(a,b) {
     console.log('홀뤼쉣')
-
+    c = arrow_function_test(a,b)
+    console.log(c)
 };
+
+
+let arrow_function_test  = (a, b) => a+b;
